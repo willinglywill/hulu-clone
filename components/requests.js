c9454c59c449ff42483693b44ad088a329f99a46
Willingly Will <anildemircoder@gmail.com>
2021-07-04 14:30:33 +0300

    api key encrypted

--- components/requests.js
+++ components/requests.js
@@ -19,82 +19,82 @@ const ids = {
 export default {
   fetchTrending: {
     name: 'Trending',
-    url: `https://api.themoviedb.org/3/trending/all/week?api_key=25e76ce48910ab5d09634318e25a43ec`,
+    url: `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.TMDB_API_KEY}`,
   },
   fetchTopRated: {
     name: 'Top Rated',
-    url: `https://api.themoviedb.org/3/movie/top_rated?api_key=25e76ce48910ab5d09634318e25a43ec&language=en-US`,
+    url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US`,
   },
 
   fetchAction: {
     actionid: 28,
     name: 'Action',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.action}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.action}`,
   },
   fetchAdventure: {
     adventureid: 12,
     name: 'Adventure',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.adventure}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.adventure}`,
   },
 
   fetchComedy: {
     comedyid: 35,
     name: 'Comedy',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.comedy}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.comedy}`,
   },
   fetchCrime: {
     crimeid: 80,
     name: 'Crime',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.crime}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.crime}`,
   },
   fetchDocumentary: {
     documentaryid: 99,
     name: 'Documentary',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.documentary}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.documentary}`,
   },
   fetchDrama: {
     dramaid: 18,
     name: 'Drama',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.drama}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.drama}`,
   },
   fetchFantasy: {
     fantasyid: 14,
     name: 'Fantasy',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.fantasy}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.fantasy}`,
   },
   fetchHistory: {
     historyid: 36,
     name: 'History',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.history}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.history}`,
   },
   fetchHorror: {
     horrorid: 27,
     name: 'Horror',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.horror}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.horror}`,
   },
   fetchMusic: {
     musicid: 10402,
     name: 'Music',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.music}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.music}`,
   },
   fetchMystery: {
     mysteryid: 9648,
     name: 'Mystery',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.mystery}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.mystery}`,
   },
   fetchRomance: {
     romanceid: 10749,
     name: 'Romance',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.romance}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.romance}`,
   },
   fetchScienceFiction: {
     scifiid: 878,
     name: 'Science-Fiction',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.scifi}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.scifi}`,
   },
   fetchWar: {
     warid: 10752,
     name: 'War',
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=25e76ce48910ab5d09634318e25a43ec&with_genres=${ids.war}`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${ids.war}`,
   },
 };
